9922f8a362d9d27d5116257df33bc5cad1a7119e
Heidenreich <flo2013hei@gmail.com>
2023-05-23 23:56:23 +0200

    not initial commit i guess

--- crui/include/crui/geometry.h
+++ crui/include/crui/geometry.h
@@ -14,35 +14,36 @@ namespace cr::ui {
     struct Window;
 
     struct Point : cr::math::cvector<float, 2> {
-        float &x() { return (*this)[0]; }
+        [[nodiscard]] float &x() { return (*this)[0]; }
 
-        float &y() { return (*this)[1]; }
+        [[nodiscard]] float &y() { return (*this)[1]; }
 
-        float x() const { return (*this)[0]; }
+        [[nodiscard]] float x() const { return (*this)[0]; }
 
-        float y() const { return (*this)[1]; }
+        [[nodiscard]] float y() const { return (*this)[1]; }
 
         Point() = default;
 
         Point(float x, float y) : cr::math::cvector<float, 2>(x, y) {}
+        Point(const cr::math::cvector<float, 2>& vec) : cr::math::cvector<float, 2>(vec) {}
     };
 
     struct Color : cr::math::cvector<float, 4> {
-        float &r() { return (*this)[0]; }
+        [[nodiscard]] float &r() { return (*this)[0]; }
 
-        float &g() { return (*this)[1]; }
+        [[nodiscard]] float &g() { return (*this)[1]; }
 
-        float &b() { return (*this)[2]; }
+        [[nodiscard]] float &b() { return (*this)[2]; }
 
-        float &a() { return (*this)[3]; }
+        [[nodiscard]] float &a() { return (*this)[3]; }
 
-        float r() const { return (*this)[0]; }
+        [[nodiscard]] float r() const { return (*this)[0]; }
 
-        float g() const { return (*this)[1]; }
+        [[nodiscard]] float g() const { return (*this)[1]; }
 
-        float b() const { return (*this)[2]; }
+        [[nodiscard]] float b() const { return (*this)[2]; }
 
-        float a() const { return (*this)[3]; }
+        [[nodiscard]] float a() const { return (*this)[3]; }
 
 
         Color() = default;
